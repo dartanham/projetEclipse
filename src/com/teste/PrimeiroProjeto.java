ca761887533090cb0cb18b4c0e8726959a9e2516
dartanham <phanton3001@gmail.com>
2021-11-24 18:11:04 -0300

    first commit

---
+++ src/com/teste/PrimeiroProjeto.java
@@ -0,0 +1,26 @@
+package com.teste;
+
+import com.teste.model.Gato;
+
+public class PrimeiroProjeto {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		Livros livros = new Livros();
+		
+		System.out.println(gato);
+		System.out.println(livros);
+		
+		/*
+		int a = 2;
+		int b = 3;
+		System.out.println("Hellow World!!" + " " + (a+b));
+		*/
+	}
+
+}
+class Livros{
+	private String nome;
+	private String npag;
+}
